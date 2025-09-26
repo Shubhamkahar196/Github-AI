e259cfccc6448a14528fb027f2b791461f3714b3
Shubham kahar <Shubhamkahar196@gmail.com>
2025-09-26 15:56:01 +0530

    Added setup clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
+// import { LatestPost } from "@/app/_components/post";
+// import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   
 
   return (
+    <div>
      <h1>Hiii</h1>
+     <Button>Click me</Button>
+    </div>
+    
   );
 }
