ae7de30b448a552aa7c7ad0dec81d31f6c13a400
Shubham kahar <Shubhamkahar196@gmail.com>
2025-09-27 21:00:06 +0530

    Added github url fetching

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,61 @@
-"use client"
+"use client";
 
-import {  useUser } from '@clerk/nextjs'
-import React from 'react'
+import useProject from "@/hooks/use-project";
+import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Dashboard = () => {
-    const {user} = useUser();
+  const { project } = useProject();
   return (
     <div>
-      <div>{user?.firstName}</div>
-       <div>{user?.lastName}</div>
-    </div>
-    
-  )
-}
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* github link */}
+        <div className="bg-primary w-fit rounded-md px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-6 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
 
-export default Dashboard
+         <div className="h-4"></div>
+
+         <div className="flex items-center gap-4">
+          Team members
+          InviteButton
+          ArchiveButton
+           </div>
+
+
+
+
+      </div>
+
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard
+          MeetingCard
+        </div>
+      </div>
+
+      <div className="mt-8">
+        commit log
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
