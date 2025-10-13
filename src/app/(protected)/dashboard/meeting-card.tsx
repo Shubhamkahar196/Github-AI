e327a3285d37208fd278ba98ba4e0ad86276413a
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-13 09:20:32 +0530

    Added process-meeting

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -12,10 +12,21 @@ import { api } from '@/trpc/react'
 import useProject from '@/hooks/use-project'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
  
 const MeetingCard = () => {
     const {project} = useProject()
-    const [progress, setProgress] = useState(0)
+   
+    const processMeeting = useMutation({
+        mutationFn: async(data: {meetingUrl: string, meetingId: string, projectId: string}) =>{
+            const {meetingUrl, meetingId, projectId} = data
+            const response = await axios.post('/api/process-meeting', {meetingUrl, meetingId, projectId})
+            return response.data
+        }
+    })
+
+     const [progress, setProgress] = useState(0)
     const router = useRouter()
     const [isUploading, setIsUploading] = useState(false)
     const uploadMeeting = api.project.uploadMeeting.useMutation()
@@ -37,9 +48,10 @@ const MeetingCard = () => {
                 meetingUrl: downloadUrl,
                 name: file.name
             },{
-                onSuccess: () =>{
+                onSuccess: (meeting) =>{
                     toast.success("Meeting uploaded successfully")
                     router.push('/meetings')
+                    processMeeting.mutateAsync({meetingUrl: downloadUrl, meetingId: meeting.id, projectId: project.id})
                 },
                 onError: ()=>{
                     toast.error("Failed to upload meeting")
