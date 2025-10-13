bc46a6768df45faaf80aa9f9fde447bb60676f43
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-13 19:37:20 +0530

    Added join

---
+++ src/app/(protected)/join/[projectId]/page.tsx
@@ -0,0 +1,60 @@
+
+
+
+import useProject from '@/hooks/use-project'
+import { auth, clerkClient, EmailAddress } from '@clerk/nextjs/server'
+import { redirect } from 'next/dist/server/api-utils'
+import React from 'react'
+
+type Props = {
+    params: Promise<{projectId: string}>
+}
+
+
+
+const JoinHandler = async(props: Props) => {
+    const {projectId} = await props.params
+    const {userId} = await auth()
+    if(!userId) return redirect("/sign-in")
+
+    const dbUser = await db.user.findUnique({
+        where:{
+            id: userId
+        }
+    })
+
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId)
+    if(!dbUser){
+        await db.user.create({
+            data:{
+ id: userId,
+            EmailAddress: user.emailAddresses[0]!.emailAddress,
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName
+            }
+            
+        })
+    }
+ 
+    const project = await db.project.findUnique({
+        where: {
+            id: projectId
+        }
+    })
+    if(!project) return redirect("/dashboard")
+        try {
+            await db.userToProject.create({
+                data: {
+                    userId,
+                    projectId
+                }
+            })
+        } catch (error) {
+            console.log('user already in project')
+        }
+        return redirect('/dashboard')
+}
+
+export default JoinHandler
